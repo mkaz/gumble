e80b8b1ea8cd0c33b267a12f62485dfff55aae73
Marcus Kazmierczak <marcus@mkaz.com>
2021-10-16 07:39:11 -0700

    Add block style theme support

--- functions.php
+++ functions.php
@@ -4,6 +4,12 @@
  * Gumble theme functions.php
  */
 
+add_action( 'init', function() {
+
+	add_theme_support( 'wp-block-styles' );
+
+} );
+
 add_action( 'wp_enqueue_scripts', function() {
 
 	// Enqueue Google font
@@ -12,13 +18,12 @@ add_action( 'wp_enqueue_scripts', function() {
 		'https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@300;500&display=swap'
 	);
 
-
 	// Enqueue theme stylesheet.
 	wp_enqueue_style(
 		'gumble-style',
 		get_template_directory_uri() . '/style.css',
 		array(),
-		wp_get_theme()->get( 'Version' )
+		filemtime( get_template_directory().'/style.css' )
 	);
 
 } );
